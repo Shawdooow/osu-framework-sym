62f9c70a3a784e87e42a08d8dcc3279fbb6fe070
Shawdooow <banhammer1915@gmail.com>
2018-09-16 08:27:33 -0400

    moved

---
+++ Symcol.Worlds/IO/TileStore.cs
@@ -0,0 +1,14 @@
+﻿using osu.Framework.Graphics.Textures;
+using osu.Framework.IO.Stores;
+using OpenTK.Graphics.ES30;
+
+namespace Symcol.Worlds.IO
+{
+    public class TileStore : TextureStore
+    {
+        public TileStore(IResourceStore<RawTexture> store = null, bool useAtlas = true, All filteringMode = All.Nearest)
+            : base(store, useAtlas, filteringMode)
+        {
+        }
+    }
+}
