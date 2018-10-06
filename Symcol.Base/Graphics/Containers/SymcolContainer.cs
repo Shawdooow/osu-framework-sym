3673ad1403eaca894bb78705b31d099cbcc46b0e
Shawdooow <banhammer1915@gmail.com>
2018-10-06 09:51:56 -0400

    update delete

--- Symcol.Base/Graphics/Containers/SymcolContainer.cs
+++ Symcol.Base/Graphics/Containers/SymcolContainer.cs
@@ -5,50 +5,22 @@ namespace Symcol.Base.Graphics.Containers
 {
     public class SymcolContainer : Container
     {
-        private bool disposed;
-
-        /// <summary>
-        /// Delete this fucking object!
-        /// </summary>
         public virtual void Delete()
         {
-            if (Parent is Container p)
-                p.Remove(this);
-
-            Dispose();
-            disposed = true;
-        }
-
-        public override bool UpdateSubTree()
-        {
-            // ReSharper disable once ConvertIfStatementToReturnStatement
-            if (disposed) return false;
-            return base.UpdateSubTree();
+            ClearTransforms();
+            ClearInternal();
+            Expire();
         }
     }
 
     public class SymcolContainer<T> : Container<T>
         where T : Drawable
     {
-        private bool disposed;
-
-        /// <summary>
-        /// Delete this fucking object!
-        /// </summary>
         public virtual void Delete()
         {
-            if (Parent is Container p)
-                p.Remove(this);
-
-            Dispose();
-            disposed = true;
-        }
-
-        public override bool UpdateSubTree()
-        {
-            // ReSharper disable once ConvertIfStatementToReturnStatement
-            if (disposed) return false;
-            return base.UpdateSubTree();
+            ClearTransforms();
+            ClearInternal();
+            Expire();
         }
     }
 }
