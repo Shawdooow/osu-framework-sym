41c96e31ab6f807749e64f6548432c1d296e3b72
Shawdooow <banhammer1915@gmail.com>
2018-10-30 08:35:51 -0400

    add some 3D structure

---
+++ Symcol.3D/_3DContainer.cs
@@ -0,0 +1,15 @@
+﻿using osu.Framework.Graphics;
+using Symcol.Base.Graphics.Containers;
+
+namespace Symcol._3D
+{
+    public class _3DContainer : SymcolContainer
+    {
+        protected override DrawNode CreateDrawNode() => new _3DDrawNode();
+
+        protected override void ApplyDrawNode(DrawNode node)
+        {
+            base.ApplyDrawNode(node);
+        }
+    }
+}
