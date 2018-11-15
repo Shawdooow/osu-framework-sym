87c5db7c58cad926931c5a12583e1fe89ab93752
Shawdooow <banhammer1915@gmail.com>
2018-11-15 15:01:58 -0500

    aspect locked container

---
+++ Symcol.Base/Graphics/Containers/AspectLockedContainer.cs
@@ -0,0 +1,30 @@
+﻿using osu.Framework.Graphics;
+using OpenTK;
+
+namespace Symcol.Base.Graphics.Containers
+{
+    public class AspectLockedContainer : AspectLockedContainer<Drawable>
+    {
+    }
+
+    public class AspectLockedContainer<T> : SymcolContainer<T>
+        where T : Drawable
+    {
+        public new float Margin = 1;
+
+        protected virtual Vector2 AspectRatio => Vector2.One;
+
+        public AspectLockedContainer()
+        {
+            Anchor = Anchor.Centre;
+            Origin = Anchor.Centre;
+        }
+
+        protected override void UpdateAfterChildren()
+        {
+            base.UpdateAfterChildren();
+
+            Scale = new Vector2(Parent.DrawSize.Y * AspectRatio.X / AspectRatio.Y / Size.X, Parent.DrawSize.Y / Size.Y) * Margin;
+        }
+    }
+}
