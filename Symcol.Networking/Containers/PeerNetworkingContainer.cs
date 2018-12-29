29ac996e34ae4854d9204ceb505d662bf8748b84
Shawdooow <banhammer1915@gmail.com>
2018-12-29 10:13:36 -0500

    que stuff

--- Symcol.Networking/Containers/PeerNetworkingContainer.cs
+++ Symcol.Networking/Containers/PeerNetworkingContainer.cs
@@ -1,4 +1,5 @@
-﻿using Symcol.Base.Graphics.Containers;
+﻿using System.Collections.Generic;
+using Symcol.Base.Graphics.Containers;
 using Symcol.Networking.NetworkingHandlers.Peer;
 using Symcol.Networking.Packets;
 
@@ -8,21 +9,41 @@ namespace Symcol.Networking.Containers
     {
         public readonly PeerNetworkingHandler PeerNetworkingHandler;
 
+        private readonly List<PacketInfo> que = new List<PacketInfo>();
+
+        private bool loaded;
+
         public PeerNetworkingContainer(PeerNetworkingHandler handler)
         {
             PeerNetworkingHandler = handler;
-            PeerNetworkingHandler.OnPacketReceive += OnPacketRecieve;
+            PeerNetworkingHandler.OnPacketReceive += onPacketReceive;
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+            loaded = true;
+            foreach (PacketInfo info in que)
+                onPacketReceive(info);
         }
 
         protected virtual void SendPacket(Packet packet) => PeerNetworkingHandler.SendToServer(packet);
 
-        protected virtual void OnPacketRecieve(PacketInfo info)
+        private void onPacketReceive(PacketInfo info)
+        {
+            if (loaded)
+                OnPacketReceive(info);
+            else
+                que.Add(info);
+        }
+
+        protected virtual void OnPacketReceive(PacketInfo info)
         {
         }
 
         protected override void Dispose(bool isDisposing)
         {
-            PeerNetworkingHandler.OnPacketReceive -= OnPacketRecieve;
+            PeerNetworkingHandler.OnPacketReceive -= onPacketReceive;
             base.Dispose(isDisposing);
         }
     }
