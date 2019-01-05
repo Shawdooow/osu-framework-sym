d7f509217792717c3d2560c058184ce26027b227
Shawdooow <banhammer1915@gmail.com>
2019-01-05 09:47:21 -0500

    start the wheel

---
+++ Symcol.Base/Touch/TouchWheel.cs
@@ -0,0 +1,39 @@
+﻿using osu.Framework.Graphics;
+using Symcol.Base.Graphics.Containers;
+
+namespace Symcol.Base.Touch
+{
+    public class TouchWheelContainer : TouchContainer
+    {
+        public TouchWheel Wheel;
+
+        public TouchWheelContainer()
+        {
+            RelativeSizeAxes = Axes.Both;
+            Width = 0.5f;
+
+            AlwaysPresent = true;
+            //Alpha = 0;
+
+            Child = Wheel = new TouchWheel
+            {
+
+            };
+        }
+
+        protected override void Tap()
+        {
+            base.Tap();
+        }
+
+        protected override void Release()
+        {
+            base.Release();
+        }
+
+        public class TouchWheel : SymcolCircularContainer
+        {
+
+        }
+    }
+}
